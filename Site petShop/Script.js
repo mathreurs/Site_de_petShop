6c7d1d9923d7c24cb82c33f1c657964a3b9bab0e
mathreurs <136806259+mathreurs@users.noreply.github.com>
2025-02-05 23:33:31 -0300

    adicionado o segredinho, imagem de cachorro e som

--- Site petShop/Script.js
+++ Site petShop/Script.js
@@ -1,3 +1,19 @@
+let num = 0;
+
 function playSound(){
     let audio = document.getElementById("sound");
+    audio.currentTime = 0;
+    audio.play();
+    num++;
+    console.log(num);
+
+    if(num == 3){
+        let cab = document.getElementById("head");
+        let cabecalho = document.getElementById("cabecalho");
+        cab.style.height = "200px";
+        cabecalho.style.height = "100px";
+
+        var hiddenImages = document.getElementById("hidden-image")
+        hiddenImages.style.display = "block";
+    }
 }
